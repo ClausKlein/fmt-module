b515252585a5b615daeec415a2daf1050689ab4f
ClausKlein <claus.klein@arcormail.de>
2024-12-04 21:44:34 +0100

    Continue try compileing module code

--- tests/header-only-test.cc
+++ tests/header-only-test.cc
@@ -1,12 +1,17 @@
 // Header-only configuration test
 
-#include "fmt/base.h"
-#include "fmt/ostream.h"
-#include "gtest/gtest.h"
-
-#ifndef FMT_HEADER_ONLY
-#  error "Not in the header-only mode."
+#ifdef FMT_USE_MODULES
+import fmt;
+#else
+#  include "fmt/base.h"
+#  include "fmt/ostream.h"
 #endif
+
+// #ifndef FMT_HEADER_ONLY
+// #  error "Not in the header-only mode."
+// #endif
+
+#include "gtest/gtest.h"
 
 TEST(header_only_test, format)
 {
