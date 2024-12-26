5ec55c23ab2ad8dc0177eeafc4385598d9e2501c
ClausKlein <claus.klein@arcormail.de>
2024-12-26 18:46:52 +0100

    Update to fmt v11.1.0

--- tests/header-only-test.cc
+++ tests/header-only-test.cc
@@ -1,20 +1,16 @@
 // Header-only configuration test
 
+#include <string_view>
+
 #ifdef FMT_MODULE
 import fmt;
 #else
 #  include "fmt/base.h"
 #  include "fmt/ostream.h"
-#  include <string_view>
 #endif
-
 
 auto main() -> int
 {
-#ifdef FMT_MODULE
-  fmt::print("OK");
-#else
   constexpr std::string_view text{"constexpr"};
   fmt::print(text);
-#endif
 }
